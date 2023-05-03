ffa593c6b245d40ecd8680bbbeb734884aece5ad
Alida Gorman <128799448+atgorman1@users.noreply.github.com>
2023-05-03 08:15:35 -0400

    Updated js to if else statement to make cat and button properties disappear and reappear

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 button.addEventListener("click", function () {
-  cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Hereeeee Kitty!";
+    button.classList.add("disappear");
+  } else {
+    cat.classList.add("show");
+    button.innerText = "Time to leave, kitty!";
+    button.classList.remove("disappear");
+  }
 });
